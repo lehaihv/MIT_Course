5ec2045fcb9ca88ffa0001319d85624f77595d99
lehaihv <69892340+lehaihv@users.noreply.github.com>
2025-05-15 16:55:18 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -12,12 +12,35 @@ import matplotlib.pyplot as plt
 import os
 os.system("clear")
 
-# Create the nodes in the graph and initialize values
-a = torch.tensor(15)
-b = torch.tensor(61)
+### Defining a dense layer ###
 
-# Add them!
-c1 = torch.add(a, b)
-c2 = a + b  # PyTorch overrides the "+" operation so that it is able to act on Tensors
-print(f"c1: {c1}")
-print(f"c2: {c2}")
+# num_inputs: number of input nodes
+# num_outputs: number of output nodes
+# x: input to the layer
+
+class OurDenseLayer(torch.nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(OurDenseLayer, self).__init__()
+        # Define and initialize parameters: a weight matrix W and bias b
+        # Note that the parameter initialize is random!
+        self.W = torch.nn.Parameter(torch.randn(num_inputs, num_outputs))
+        self.bias = torch.nn.Parameter(torch.randn(num_outputs))
+
+    def forward(self, x):
+        '''TODO: define the operation for z (hint: use torch.matmul).'''
+        z = torch.matmul(x,self.W)
+
+        '''TODO: define the operation for out (hint: use torch.sigmoid).'''
+        y = torch.sigmoid(z + self.bias)
+        return y
+
+# Define a layer and test the output!
+num_inputs = 2
+num_outputs = 3
+layer = OurDenseLayer(num_inputs, num_outputs)
+x_input = torch.tensor([[1, 2.]])
+y = layer(x_input)
+
+print(f"input shape: {x_input.shape}")
+print(f"output shape: {y.shape}")
+print(f"output result: {y}")
